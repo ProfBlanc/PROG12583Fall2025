d8580d52debd207a9dd8d173d9fc88a18fdf9f18
Prof Blanc <ben.blanc@georgebrown.ca>
2025-10-31 08:20:47 -0400

    Class Code prep part 2 Oct 31 @ 8h20

--- wk8/fri/a2_prep.py
+++ wk8/fri/a2_prep.py
@@ -1,12 +1,69 @@
 def get_people_avg_height(people):
     """
-    Get's the average height of person
+    Get's the average height of people
 
     >>> get_people_avg_height({'Ben': 110, 'John': 120, 'Mary': 130})
     >>> 120
     """
     final_value = ""
 
+    ################# Start of Code #################
     final_value = sum(people.values()) / len(people.keys())
+    ################# End of Code  ##################
+    return final_value
 
+def get_min_people_height(people):
+    """
+    Get's the min height of people
+
+    >>> get_min_people_height({'Ben': 110, 'John': 120, 'Mary': 130})
+    >>> 110
+    """
+    final_value = ""
+
+    ################# Start of Code #################
+    
+    ################# End of Code  ##################
+    return final_value
+
+def get_max_people_height(people):
+    """
+    Get's the max height of people
+
+    >>> get_max_people_height({'Ben': 110, 'John': 120, 'Mary': 130})
+    >>> 130
+    """
+    final_value = ""
+
+    ################# Start of Code #################
+    
+    ################# End of Code  ##################
+    return final_value
+
+def get_tallest_person(people):
+    """
+    Get's the tallest person
+
+    >>> get_tallest_person({'Ben': 110, 'John': 120, 'Mary': 130})
+    >>> 'Mary'
+    """
+    final_value = ""
+
+    ################# Start of Code #################
+    
+    ################# End of Code  ##################
+    return final_value
+
+def get_shortest_person(people):
+    """
+    Get's the shortest person
+
+    >>> get_shortest_person({'Ben': 110, 'John': 120, 'Mary': 130})
+    >>> 'Ben'
+    """
+    final_value = ""
+
+    ################# Start of Code #################
+    
+    ################# End of Code  ##################
     return final_value

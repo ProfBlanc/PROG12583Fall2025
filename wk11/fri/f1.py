7ada75050bba687ff712670f3036b4222b1424c2
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2025-11-21 09:28:34 -0500

    Create f1.py

---
+++ wk11/fri/f1.py
@@ -0,0 +1,23 @@
+"""
+A class is an organizational structure
+
+Combine various components of programming
+    Module
+    Variables
+    Functions
+
+Why use classes?
+    A template or prototype that is used to create or replicate
+    an object
+    
+Class has 2 aspects
+    Creating the model/template/prototype
+    Create an instance/object of the class
+    
+"""
+# aspect 1
+class Basic:
+    pass
+# aspect 2
+b1 = Basic()
+print(b1)
